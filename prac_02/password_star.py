81ba49a085ed8bfbedfb8d5d7e1c39dbd71485a9
KennardLim <kennard.lim@my.jcu.edu.au>
2025-02-04 23:27:00 +0800

    Refactor password check program to use functions

--- prac_02/password_star.py
+++ prac_02/password_star.py
@@ -1,7 +1,17 @@
 MINIMUM_PASSWORD_LENGTH = 10
 
-password = input(f"Make a password (minimum {MINIMUM_PASSWORD_LENGTH} characters):  ")
-while len(password) < MINIMUM_PASSWORD_LENGTH:
-    print("Password is not long enough!")
+def main():
+    password = get_password()
+    print_into_asterisks(password)
+
+def get_password():
     password = input(f"Make a password (minimum {MINIMUM_PASSWORD_LENGTH} characters):  ")
-print("*" * len(password))
+    while len(password) < MINIMUM_PASSWORD_LENGTH:
+        print("Password is not long enough!")
+        password = input(f"Make a password (minimum {MINIMUM_PASSWORD_LENGTH} characters):  ")
+    return password
+
+def print_into_asterisks(word):
+    print("*" * len(word))
+
+main()
