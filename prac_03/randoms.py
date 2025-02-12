612418a2affe019ac315752279a36422962cb8f4
KennardLim <kennard.lim@my.jcu.edu.au>
2025-02-12 17:45:39 +0800

    Add answers to the questions section

---
+++ prac_03/randoms.py
@@ -0,0 +1,19 @@
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+    # A line to print random number between 5 and 20 inclusive
+    # The smallest number possible was 5 and the largest number possible was 20
+import random
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+    # A line to print a random number from a sequence in range of 3 to 10 with step count of 2
+    # The smallest number possible was 5 and the largest number possible was 9
+    # No, because the step count was 2 meaning the only possible numbers were 5, 7, or 9
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+    # A line to print random float point number (decimal) between 2.5 and 5.5 inclusive
+    # The smallest number possible was 2.5 and the largest number possible was 5.5
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
