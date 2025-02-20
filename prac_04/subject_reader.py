8307ab6799125b5d249a46d38a3c63963593cc5c
KennardLim <kennard.lim@my.jcu.edu.au>
2025-02-20 12:34:48 +0800

    Modify load_data function to return data as a nested list, and add print_subject_detail to print the elements of data in the requested format

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,10 +9,11 @@ FILENAME = "subject_data.txt"
 def main():
     data = load_data()
     print(data)
-
+    print_subject_detail(data)
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -22,8 +23,16 @@ def load_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        data += [parts]
         print("----------")
     input_file.close()
+    return data
 
+def print_subject_detail(data):
+    """Print each subject detail in data in a sentenced format"""
+    max_lecturer_length = len(max([subject_detail[1] for subject_detail in data], key=len))
+    max_number_of_students_length = len(max([str(subject_detail[2]) for subject_detail in data], key=len))
+    for subject_detail in data:
+        print(f"{subject_detail[0]} is taught by {subject_detail[1]:{max_lecturer_length}} and has {subject_detail[2]:{max_number_of_students_length}} students")
 
 main()
