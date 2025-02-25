21e01d75e7bdd00235c8af97b8e766e2cb83ac04
KennardLim <kennard.lim@my.jcu.edu.au>
2025-02-25 22:51:45 +0800

    Remove leftover unused import statement and print statements from word_occurrences

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,11 +3,8 @@ Word Occurrences
 Estimate: 20 minutes
 Actual: 32 minutes
 """
-from operator import itemgetter
-
 words = input("Text: ").split()
 word_to_count = dict()
-print(word_to_count)
 for word in words:
     if word in word_to_count:
         word_to_count[word] += 1
