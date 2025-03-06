b812985cb1a96488a4b9c70810820257b1220dc6
KennardLim <kennard.lim@my.jcu.edu.au>
2025-03-06 12:54:07 +0800

    Complete Intermediate Exercises

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,5 +1,18 @@
 """
 Intermediate Exercises
 Estimate: 30 minutes
-Actual:
+Actual: 17 minutes
 """
+
+from prac_06.programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
+
+programming_languages = [python, ruby, visual_basic]
+dynamic_programming_languages = [programming_language for programming_language in programming_languages if programming_language.is_dynamic()]
+print(f"The dynamically typed languages are:")
+for dynamic_programming_language in dynamic_programming_languages:
+    print(dynamic_programming_language.name)
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,5 +1,19 @@
 """
 Intermediate Exercises
 Estimate: 30 minutes
-Actual:
+Actual: 17 minutes
 """
+
+class ProgrammingLanguage:
+
+    def __init__(self, name="", type="", reflection="", year=""):
+        self.name = name
+        self.type = type
+        self.reflection = bool(reflection)
+        self.year = int(year)
+
+    def __str__(self):
+        return f"{self.name}, {self.type} typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        return self.type == "Dynamic"
