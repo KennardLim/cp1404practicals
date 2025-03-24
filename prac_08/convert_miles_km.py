eff4b404c09bbb92b1598711409c6d47562d57f9
KennardLim <kennard.lim@my.jcu.edu.au>
2025-03-24 22:28:15 +0800

    Finish Stage 2/ Part 2 of Miles to Kilometres Converter Made the app use StringProperty for the output_label

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,19 +1,25 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 KM_PER_MILE = 1.60934
 DEFAULT_VALUE = 0
 
 class ConvertMilesToKilometresApp(App):
+    """MilesConverterApp is a Kivy App for converting miles to kilometres"""
+    result = StringProperty()
+
     def build(self):
         """Build the Kivy app from the kv file"""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file("convert_miles_km.kv")
+        self.result = "54.717"
         return self.root
 
     def handle_calculate(self):
         """Handle calculation (could be button press or other call), output result to label widget"""
-        self.root.ids.output_label.text = str(self.get_valid_input() * KM_PER_MILE)
+        # self.root.ids.output_label.text = self.get_valid_input() * KM_PER_MILE
+        self.result = str(self.get_valid_input() * KM_PER_MILE)
 
     def handle_increment(self, change):
         """Handle up/down button, updates the text input with the changed value
