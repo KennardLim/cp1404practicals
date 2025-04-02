58fe3c6086dbdf11a779f7de0c0cddc66aedd74a
KennardLim <kennard.lim@my.jcu.edu.au>
2025-04-02 15:41:54 +0800

    Modified get_fare method in Taxi class to round up to 10c Modified silver_service_taxi_test.py to test the modified get_fare method Complete Inheriting Enhancements

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -4,13 +4,12 @@ fancy_taxi = SilverServiceTaxi("The Shining Chariot", 100, 2.0)
 print(f"\n{fancy_taxi}")
 
 print(f"{fancy_taxi.name} drove {fancy_taxi.drive(18)}km")
-assert fancy_taxi.get_fare() == 48.78, "Should be $48.78"
-print(fancy_taxi.get_fare())
+assert fancy_taxi.get_fare() == 48.8, "Should be $48.8"
+print(f"{fancy_taxi.get_fare():,.2f}")
 
-president_taxi = SilverServiceTaxi("Presidentail carrier", 105, 4.0)
+president_taxi = SilverServiceTaxi("Presidential carrier", 105, 4.0)
 print(f"\n{president_taxi}")
 
 print(f"{president_taxi.name} drove {president_taxi.drive(30)}km")
 assert president_taxi.get_fare() == 152.10, "Should be $152.10"
-print(president_taxi.get_fare())
-
+print(f"{president_taxi.get_fare():,.2f}")
