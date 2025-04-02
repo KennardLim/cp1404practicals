83b93ef6076e550a5ba561a84c91c5df2009feca
KennardLim <kennard.lim@my.jcu.edu.au>
2025-04-02 20:29:18 +0800

    Finish creating taxi_simulator.py Complete Do-from-scratch Exercises

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,52 @@
+from ast import Index
+
+from prac_09.taxi import Taxi
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+TAXIS = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+def main():
+    current_taxi = None
+    total_bill = 0.0
+    print("Let's drive!")
+    print(MENU)
+    choice = input(">>> ").lower()
+    while choice != "q":
+        if choice == "c":
+            display_taxis(TAXIS)
+            current_taxi = choose_taxi(TAXIS)
+        elif choice == "d":
+            total_bill += drive_taxi(current_taxi)
+        else:
+            print("Invalid option")
+        print(f"Bill to date: ${total_bill:,.2f}")
+        print(MENU)
+        choice = input(">>> ").lower()
+    print(f"Total trip cost: ${total_bill:,.2f}")
+    print("Taxis are now:")
+    display_taxis(TAXIS)
+
+def display_taxis(taxis):
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+
+def choose_taxi(taxis):
+    taxi_choice = int(input("Choose taxi: "))
+    try:
+        return taxis[taxi_choice]
+    except IndexError:
+        print("Invalid taxi choice")
+
+def drive_taxi(taxi):
+    try:
+        taxi.start_fare()
+        drive_distance = int(input("Drive how far? "))
+        taxi.drive(drive_distance)
+        print(f"Your {taxi.name} trip cost you {taxi.get_fare():,.2f}")
+        return taxi.get_fare()
+    except AttributeError:
+        print("You need to choose a taxi before you can drive")
+        return 0
+
+main()
